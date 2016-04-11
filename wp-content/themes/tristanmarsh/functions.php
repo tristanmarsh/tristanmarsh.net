10a5cfc9f82fb286a303bab54c3057197a54afa7
Tristan Marsh <tristanmarsh@live.com>
2016-04-11 22:48:29 +1000

    Social Media Icons Added
    facebook
    instagram
    github
    codepen
    teamtreehouse
    twitter
    googleplus

--- wp-content/themes/tristanmarsh/functions.php
+++ wp-content/themes/tristanmarsh/functions.php
@@ -5,7 +5,9 @@ function tristanmarsh_scripts() {
   // wp_enqueue_style( 'bootstrap-core', get_template_directory_uri() . '/resources/css/bootstrap.min.css' );
 
   // Load main theme stylesheet
-  wp_enqueue_style( 'tristanmarsh-style', get_stylesheet_uri() );
+  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/resources/css/bootstrap.min.css' );
+  wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
+  wp_enqueue_style( 'tristanmarsh-css', get_stylesheet_uri() );
 
   // Additional Elements
   // wp_enqueue_style( 'animate-css', get_stylesheet_directory_uri() . '/resources/css/animate.min.css' );
@@ -14,10 +16,10 @@ function tristanmarsh_scripts() {
   wp_enqueue_script('jquery');
 
     // Load main theme script
-  wp_enqueue_script( 'core', get_template_directory_uri() . '/resources/js/min/core-min.js', array(), false );
+  wp_enqueue_script( 'tristanmarsh-js', get_template_directory_uri() . '/resources/js/min/core-min.js', array(), false );
 
   // Load Bootstrap
-  wp_enqueue_script('bootstrap', get_template_directory_uri() . '/resources/js/min/bootstrap.min.js', true);
+  wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/resources/js/min/bootstrap.min.js', true);
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
